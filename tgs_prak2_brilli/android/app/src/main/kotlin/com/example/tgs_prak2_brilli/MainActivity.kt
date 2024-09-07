a498f93a5b7396e8defb6508a0751c790ed8e1f9
Brilliantna Salsabila <sbrilliantna@gmail.com>
2024-09-07 12:49:03 +0700

    first commit

---
+++ tgs_prak2_brilli/android/app/src/main/kotlin/com/example/tgs_prak2_brilli/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tgs_prak2_brilli
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tgs_prak2_brilli/lib/main.dart
@@ -0,0 +1,5 @@
+void main(){
+  for (int i = 0; i < 10; i++){
+    print('Nama saya Brilli, sekarang berumur ${i + 2}');
+  }
+}
